741637c48d271a9f3cab606b5630e5632bbb8d76
Quinn Meurer <quinnyyy@bu.edu>
2020-11-03 17:44:51 -0500

    Remove test path

--- stod-backend/stod/groups/urls.py
+++ stod-backend/stod/groups/urls.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 from .views import GroupViewset
 
 router = routers.DefaultRouter()
-router.register("test", GroupViewset, "group")
+router.register("", GroupViewset, "group")
 
 urlpatterns = router.urls

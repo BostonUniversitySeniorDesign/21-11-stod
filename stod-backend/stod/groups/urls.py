3f4c377cf1de9d038517e899ca2d10a7d1cc7ba8
Quinn Meurer <quinnyyy@bu.edu>
2020-11-03 16:34:29 -0500

    Update comment string

--- stod-backend/stod/groups/urls.py
+++ stod-backend/stod/groups/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('create', views.create, name='create'),
+    path('', views.getGroups, name='getGroups'),
 ]
--- stod-backend/stod/groups/views.py
+++ stod-backend/stod/groups/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, JsonResponse
 import json
 
 from .models import Group
@@ -30,3 +30,19 @@ def create(request):
     newGroup.save()
 
     return HttpResponse(status=200)
+
+
+def getGroups(request):
+    """
+    GET endpoint to get all groups.
+    Returns a JSON response.
+    """
+    if request.method != "GET":
+        return HttpResponse(status=404)
+
+    groups = Group.objects.all()
+    response = []
+    for group in groups:
+        response.append({'name': group.name, 'description': group.description})
+
+    return JsonResponse({'groups': response})
